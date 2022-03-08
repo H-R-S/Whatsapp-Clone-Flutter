d70c60645b17d1a133ac79b88433a77706802d2b
Muhammad Haris <369haris@gmail.com>
2022-03-08 13:19:01 +0500

    WhatsAPP Clone APP UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-import 'screens/home.dart';
+import 'home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Whatsapp Clone',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: HomeScreen(),
     );
   }
