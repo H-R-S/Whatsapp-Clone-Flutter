8e856c5e7cefed374c7bde908dad4775e3130115
Muhammad Haris <369haris@gmail.com>
2022-03-10 00:25:14 +0500

    MyAppBar Widget added

--- lib/home.dart
+++ lib/home.dart
@@ -1,40 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/main.dart';
 import 'package:whatsapp_clone/screens/call_screen.dart';
 import 'package:whatsapp_clone/screens/chats_screen.dart';
 import 'package:whatsapp_clone/screens/status_screen.dart';
+import 'package:whatsapp_clone/widgets/app_bar/my_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: SafeArea(
+        length: 3,
         child: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: const Color(0xff128c7e),
-            bottom: const TabBar(
-              indicatorColor: Colors.white,
-              tabs: [
-                Tab(text: "CHATS"),
-                Tab(text: "STATUS"),
-                Tab(text: "CALLS"),
-              ],
-            ),
-            title: const Text('WhatssApp'),
-            actions: const [
-              Icon(Icons.search),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Icon(Icons.more_vert),
-              )
-            ],
-          ),
+            body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [MyAppBar()];
+          },
           body: TabBarView(
             children: [ChatsScreen(), StatusSCreen(), CallScreen()],
           ),
-        ),
-      ),
-    );
+        )));
   }
 }
