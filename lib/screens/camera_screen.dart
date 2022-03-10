653b9add831dee42a203a7a7af6200f2a2b247c1
Muhammad Haris <369haris@gmail.com>
2022-03-10 23:54:28 +0500

    Camera Screen Added

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/camera/my_camera.dart';
+
+class CameraScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyCamera();
+  }
+}
