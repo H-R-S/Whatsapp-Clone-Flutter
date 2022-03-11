072520d8d8a8cb07a91ac41c146ed398092227b3
Muhammad Haris <369haris@gmail.com>
2022-03-11 23:57:18 +0500

    Color constants added

---
+++ lib/constants/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(19, 28, 33, 1);
+const textColor = Color.fromRGBO(241, 241, 242, 1);
+const appBarColor = Color.fromRGBO(31, 44, 52, 1);
+const messageColor = Color(0xffd9fdd3);
+const senderMessageColor = Colors.white;
+const tabColor = Color(0xff128c7e);
+const searchBarColor = Color.fromRGBO(50, 55, 57, 1);
+const dividerColor = Color.fromRGBO(37, 45, 50, 1);
+const chatBarMessage = Color.fromRGBO(30, 36, 40, 1);
+const mobileChatBoxColor = Color.fromRGBO(31, 44, 52, 1);
