2ed64141ae2190786ccea8b4dabac2fc55734de5
Muhammad Haris <369haris@gmail.com>
2022-03-12 00:20:17 +0500

    User profile image updated

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -4,83 +4,64 @@ const user_info = [
     'name': 'WhatsApp',
     'message': 'Welcome to Whatsapp clone.',
     'time': '7:53 pm',
-    'profilePic': 'assets/logos/whatsapp_logo.png',
+    'profilePic':
+        'https://i.pinimg.com/736x/92/4f/3c/924f3c760e37afd50a342b7903cfc0e6.jpg',
   },
   {
     'name': 'Haris',
     'message': 'Assalamualikum',
     'time': '12:53 am',
-    'profilePic': 'assets/images/my_image.jpg',
+    'profilePic':
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
   {
-    'name': 'Rivaan Ranawat',
+    'name': 'Ali',
     'message': 'Hey, how are you doing?',
     'time': '3:53 pm',
     'profilePic':
-        'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
   {
-    'name': 'John Doe',
+    'name': 'Ahmed',
     'message': 'Hello, whats up',
     'time': '2:25 pm',
     'profilePic':
-        'https://www.socialketchup.in/wp-content/uploads/2020/05/fi-vill-JOHN-DOE.jpg',
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
   {
-    'name': 'Naman Ranawat',
-    'message': 'Hello, I want to sleep.',
+    'name': 'Amir Liaquat',
+    'message': 'Aam khayega?',
     'time': '1:03 pm',
     'profilePic':
-        'https://media.cntraveler.com/photos/60596b398f4452dac88c59f8/16:9/w_3999,h_2249,c_limit/MtFuji-GettyImages-959111140.jpg',
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
   {
-    'name': 'Dad',
+    'name': 'Friend',
     'message': 'Call me, have some work',
     'time': '12:06 pm',
     'profilePic':
-        'https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg',
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
   {
-    'name': 'Mom',
-    'message': 'You ate food?',
+    'name': 'Hammad',
+    'message': 'Assalamualikum',
     'time': '10:00 am',
     'profilePic':
-        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
   {
-    'name': 'Jurica',
-    'message': 'Yo!!!!! Long time, no see!?',
+    'name': 'Humza',
+    'message': 'Uni kab chalna hai?',
     'time': '9:53 am',
     'profilePic':
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
   {
-    'name': 'Albert Dera',
-    'message': 'Am I fat?',
+    'name': 'Imran Khan',
+    'message': 'Ghabrana nhi hai',
     'time': '7:25 am',
     'profilePic':
-        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-  },
-  {
-    'name': 'Joseph',
-    'message': 'I am from International Olym...',
-    'time': '6:02 am',
-    'profilePic':
-        'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-  },
-  {
-    'name': 'Sikandar',
-    'message': 'Lets Code!',
-    'time': '4:56 am',
-    'profilePic':
-        'https://images.unsplash.com/photo-1619194617062-5a61b9c6a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
-  },
-  {
-    'name': 'Ian Dooley',
-    'message': 'Images by Unsplash',
-    'time': '1:00 am',
-    'profilePic':
-        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+        'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-default-avatar-profile-icon-vector-social-media-user-image-vector-illustration-227787227.jpg',
   },
 ];
 
@@ -88,33 +69,8 @@ const messages = [
   {"isMe": false, "text": "Assalamualikum", "time": "10:00 am"},
   {"isMe": true, "text": "WalikumAsalam", "time": "11:00 am"},
   {"isMe": false, "text": "What are you doing", "time": "11:01 am"},
-  {
-    "isMe": false,
-    "text": "This is whatsapp clone",
-    "time": "11:01 am"
-  },
+  {"isMe": false, "text": "This is whatsapp clone", "time": "11:01 am"},
   {"isMe": true, "text": "Good Work", "time": "11:03 am"},
-  {
-    "isMe": false,
-    "text": "Noice",
-    "time": "11:04 am"
-  },
-  {
-    "isMe": true,
-    "text": "Thanks",
-    "time": "11:05 am"
-  },
-  {
-    "isMe": false,
-    "text": "Haris",
-    "time": "11:06 am",
-  },
-  {
-    "isMe": true,
-    "text": "Looks great",
-    "time": "11:15 am",
-  },
-  {"isMe": false, "text": "Thanks bro!", "time": "11:17 am"},
   {"isMe": false, "text": "No Problem", "time": "11:16 am"},
   {"isMe": true, "text": "Ok bro!", "time": "11:17 am"},
   {
